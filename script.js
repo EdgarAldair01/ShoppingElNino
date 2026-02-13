9e73d6b078f3c53ef6fb9c84e3bcca41678a0d25
EdgarAldair01 <119363875+EdgarAldair01@users.noreply.github.com>
2026-02-13 12:00:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -150,13 +150,6 @@ const perfumes = [
         precio: 600,
         familia: "Cítrico",
         imagen: "IMAGENES\\Nautica Voyage.png"
-    },{
-        id: 22,
-        nombre: "Fakhar Black",
-        marca: "Bergamota Cedro",
-        precio: 900,
-        familia: "Amaderada",
-        imagen: "IMAGENES\\Fakhar Black.png"
     }
 ];
 
@@ -304,4 +297,5 @@ document.getElementById('searchInput').addEventListener('input', (e) => {
 });
 
 // INICIALIZAR
+
 renderProducts();
